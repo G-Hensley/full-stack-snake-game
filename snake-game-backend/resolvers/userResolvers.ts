67e0bbc44cb78d2336615586f5f7a1312f1b976f
Gavin Hensley <gavinhensley@protonmail.com>
2025-08-12 16:16:44 -0400

    Set up resolver for users

---
+++ snake-game-backend/resolvers/userResolvers.ts
@@ -0,0 +1,20 @@
+const userResolvers = (user?) => ({
+  Query: {
+    user: {
+      id: user?.id,
+      username: user?.username,
+      password_hash: user?.password_hash
+    }
+  },
+
+  Mutation: {
+    createUser: (parent, { input }) => {
+      const newUser = {
+        username: input.username,
+        password_hash: input.password_hash
+      };
+      // Logic to save the new user to the database
+      return newUser;
+    }
+  }
+});
